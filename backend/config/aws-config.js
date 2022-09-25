891600a693876ff00f920c23f8bac0f091040814
AleksaRadovanovic <88893903+AleksaRadovanovic@users.noreply.github.com>
2022-09-25 22:37:01 +0200

    Update aws-config.js

--- backend/config/aws-config.js
+++ backend/config/aws-config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    "region": "eu-central-1",
-    "endpoint": "http://dynamodb.eu-central-1.amazonaws.com",
-    "accessKeyId": "AKIAZJFGRM2V4S2A2TH6", 
-    "secretAccessKey": "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"
+    "region": "",
+    "endpoint": "",
+    "accessKeyId": "", 
+    "secretAccessKey": ""
   };
   
